3984defdb8579b9e68a24229bd25aa8dbbc37397
stephannybonete <stephanny.bonete@escola.pr.gov.br>
2025-02-17 08:32:34 -0300

    Add files via upload

---
+++ nome.js
@@ -0,0 +1,6 @@
+const nome = "Stephanny";
+const sobreNome = "Bonete"
+const nomeCompleto = nome + " " + sobreNome;
+
+console.log(nome,sobreNome);
+console.log(nomeCompleto);
---
+++ soma.js
@@ -0,0 +1,6 @@
+const primeiroNumero = 1;
+const segundoNumero = 9;
+
+const operacaoMatematica = primeiroNumero+segundoNumero
+
+console.log(operacaoMatematica)
