21d8d31a708dc473d875e5ecc520126679be51dc
stephannybonete <stephanny.bonete@escola.pr.gov.br>
2025-05-29 08:08:31 -0300

    Delete Grabriel directory

--- Grabriel/php/src/Classe/Fornecedor.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class fornecedor
-{
-    public const PAIS = "Brasil";
-    public $razaoSocial;
-    public $nomeFantasia;
-    public $cnpj;
-}
--- Grabriel/php/src/Classe/Produto.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class produto
-{
-    public const NORMA = "1050";
-    public $titulo;
-    public $descricao = "Carro";
-    public $preco;
-}
